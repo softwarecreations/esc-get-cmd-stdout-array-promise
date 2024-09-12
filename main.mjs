24ac4c84a26be03108bd80c82b9c3087d6928948
Joshua Maserow <joshua@softwarecreations.co.za>
2024-09-12 10:28:38 +0200

    'v0.1.0 code'

--- main.mjs
+++ main.mjs
@@ -1,8 +1,23 @@
-#!/usr/bin/env node
 'use strict';
 
-const fooP = async () => {
+import { exec } from 'child_process';
+import { promisify } from 'util';
+const execP = promisify(exec);
 
-};
+const getCmdStdoutAP = (cmd, options) => new Promise( (resolveF, rejectF) => {
+  execP(cmd, options)
+  .then( ({ stdout }) => {
+    stdout = stdout.trim();
+    resolveF(stdout.length===0 ? [] : stdout.split('\n').map(s => s.trim()))
+  })
+  .catch( err => {
+    const { stdout, stderr, code } = err;
+    switch (onError) {
+      case 'empty': return resolveF([]);
+      case 'exit' : return exitError(`command ${colors.magenta(cmd)} failed with code ${code}\nstdout: ${stdout}\nstderr: ${stderr}\n`);
+      default     : return rejectF(err);
+    }
+  });
+});
 
-export default fooP;
+export default getCmdStdoutAP;
